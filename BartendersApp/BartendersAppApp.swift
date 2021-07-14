d5505d87cf5e122e7a65b82bf5fd793378a398c4
Kirthi Maharaj <kirthimaharaj@glucode.com>
2021-07-14 19:19:28 +0200

    initial commit

---
+++ BartendersApp/BartendersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BartendersAppApp.swift
+//  BartendersApp
+//
+//  Created by Kirthi Maharaj on 2021/07/14.
+//
+
+import SwiftUI
+
+@main
+struct BartendersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
